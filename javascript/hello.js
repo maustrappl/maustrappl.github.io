dbe8f31c9ac6cb1b1ebf21537685309ce3a1394d
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-03-26 16:05:58 +0100

    Strings in Javascript

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,22 @@
-alert("Hello world");
+// alert("Hello world");
+
+// let message = "Hello world";
+// alert(message);
+// message = "Hallo Welt";
+// alert(message);
+
+const LINK_COLOR = "#ff0000"
+console.log("link bitte in der Farbe", LINK_COLOR);
+
+let highscore = 520533;
+console.log(highscore / 10);
+
+let firstname = "John"
+let lastname = "Smith"
+console.log("Name", firstname, lastname);
+
+let fullname = 'Jeffrey" "the Dude" Lebowski';
+console.log(fullname)
+
+let template = 'Dein Highscore sind ${highscore} Punkte';
+console.log(template);
