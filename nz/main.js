235aec77b0953bee393d5b21bcbe85f4eb23f271
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-03-30 19:49:02 +0200

    popup mit html verlinken

--- nz/main.js
+++ nz/main.js
@@ -6,7 +6,15 @@ let lng = map.dataset.lng;
 var mymap = L.map('map').setView([lat,lng], 13);
 
 
+let title =map.dataset.title;
+console.log(title)
+
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
+
+ let marker = L.marker([lat,lng]).addTo(mymap);
+
+ marker.bindPopup(title).openPopup(); //titel nicht in anführubngszeichen  um in html zu schreiben
+ 
