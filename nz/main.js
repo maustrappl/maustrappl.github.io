0d16458a9467ef43600a2728f9e64ce6bb733ebd
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-03-30 10:24:29 +0200

    zur karte hinzufügen

--- nz/main.js
+++ nz/main.js
@@ -4,3 +4,9 @@ let lat = map.dataset.lat;
 let lat = map.dataset.lng;
 
 var mymap = L.map(`map`).steView([lat,lng], 13);
+
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
