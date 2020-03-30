c6e8907eb56d726ebced0dfe56af61fdc1a39add
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-03-30 09:29:09 +0200

    einbinden

--- nz/main.js
+++ nz/main.js
@@ -1,13 +1,12 @@
 //alert("Hallo Welt!")
-let map = document.querySelector("map")
+let map = document.querySelector("#map")
+let lat = map.dataset.lat;
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+var mymap = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
 
-var marker = L.marker([-45.8909481029, 170.6417191]).addTo(mymap);
-
-marker.bindPopup("<b>Doubtfull Sound</b><br>a D").openPopup();
