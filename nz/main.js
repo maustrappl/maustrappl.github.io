b10227bf606483185c9bb8b6eba820dff5aa099e
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-03-30 10:20:57 +0200

    verweis auf lage im html sheet

--- nz/main.js
+++ nz/main.js
@@ -1,4 +1,6 @@
 //alert("Hallo Welt!")
 let map = document.querySelector("#map")
+let lat = map.dataset.lat;
+let lat = map.dataset.lng;
 
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+var mymap = L.map(`map`).steView([lat,lng], 13);
