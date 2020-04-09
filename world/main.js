1eedd9c8503d3414c984120b2a7ee1fe77b27d60
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-04-09 16:09:49 +0200

    refill

--- world/main.js
+++ world/main.js
@@ -1,4 +1,4 @@
-let startLayer = L.tileLayer.provider("OpenTopoMap");
+let startLayer = L.tileLayer.provider("Esri.WorldTopoMap");
 
 let map = L.map("map", {
     center: [0, 0],
@@ -9,24 +9,37 @@ let map = L.map("map", {
 });
 
 let circleGroup = L.featureGroup().addTo(map);
+
 L.control.layers({
-    "OpenTopoMap": startLayer,
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
     "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "NASAGIBS.ModisTerraTrueColorCR": L.tileLayer.provider("NASAGIBS.ModisTerraTrueColorCR"), //http://leaflet-extras.github.io/leaflet-providers/preview/index.html
-    "Stamen.Toner": L.tileLayer.provider("Stamen.Toner"),
-    "Stamen.TerrainLabels": L.tileLayer.provider("Stamen.TerrainLabels"),
+    "Stamen.TonerLite": L.tileLayer.provider("Stamen.TonerLite"),
+    "Stamen.Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
+    "Stamen.Terrain": L.tileLayer.provider("Stamen.Terrain"),
+    "Stamen.TerrainBackground": L.tileLayer.provider("Stamen.TerrainBackground"),
+    "Esri.WorldStreetMap": L.tileLayer.provider("Esri.WorldStreetMap"),
+    "Esri.WorldTopoMap": startLayer,
     "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+    "Esri.WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
+    "Esri.WorldGrayCanvas": L.tileLayer.provider("Esri.WorldGrayCanvas"),
+    "CartoDB.Positron": L.tileLayer.provider("CartoDB.Positron")
 }, {
     "Thematische Darstellung": circleGroup
 }).addTo(map);
 
-let drawCircles = function (data) {
+let drawCircles = function () {
     let data = CONFIRMED;
     let header = CONFIRMED[0];
-    let index = header.length -1;
-    let topic = "bestätigte Fälle";
-// datum und Thema anzeigen
-    document.querySelector("#datu").innerHTML = `am${header[index]}- ${topic}`;
+    let index = header.length - 1;
+    let options = document.querySelector("#pulldown").options;
+    let value = options[options.selectedIndex].value;
+    let label = options[options.selectedIndex].text;
+    console.log(value,label,options);
+
+    // Datum & Thema anzeigen anzeigen
+    document.querySelector("#datum").innerHTML = `am ${header[index]} - ${label}`;
+
+    //console.log(data);
     for (let i = 1; i < data.length; i++) {
         let row = data[i];
         //console.log(row[2],row[3]);
@@ -34,7 +47,12 @@ let drawCircles = function (data) {
         let lat = row[2];
         let lng = row[3];
         let val = row[index];
+        //let mrk = L.marker([lat,lng]).addTo(map);
+        //mrk.bindPopup(`${reg}: ${val}`);
 
+        //A = r²*PI
+        //r² = A/PI
+        //r = WURZEL(A/PI)
         let s = 0.5;
         let r = Math.sqrt(val * s / Math.PI);
         let circle = L.circleMarker([lat, lng], {
@@ -42,6 +60,10 @@ let drawCircles = function (data) {
         }).addTo(circleGroup);
         circle.bindPopup(`${reg}: ${val}`);
     }
+};
+
+document.querySelector("#pulldown").onchange = function() {
+    drawCircles();
 };
 
 drawCircles();
