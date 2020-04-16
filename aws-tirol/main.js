4a48367061a112d58670d864534571f001d900c0
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-04-16 18:16:07 +0200

    filter over 3000

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -30,13 +30,19 @@ let awsUrl = "https://aws.openweb.cc/stations";
 
 
 let aws = L.geoJson.ajax(awsUrl, {
+    filter: function(feature) {
+        console.log("Feature in filter: ", feature);
+        return feature.geometry.coordinates[2] > 3000;
+    },
     pointToLayer: function(point, latlng) {
-        console.log("point: ", point);
+        // console.log("point: ", point);
         let marker = L.marker(latlng).bindPopup(`
-        <h3>${point.properties.name}</h3>
+        <h3>${point.properties.name} ${point.geometry.coordinates[2]} m</h3>
         <ul>
+        <li>Position: Lat: ${point.geometry.coordinates[1]}/Lng: ${point.geometry.coordinates[0]}</li>
         <li>Datum: ${point.properties.date}</li>
         <li>Lufttemperatur: ${point.properties.LT} °C</li>
+        <li>Windgeschwindigkeit: ${point.properties.WG} m/s</li>
         </ul>
         `);
         return marker;
