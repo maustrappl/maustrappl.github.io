436c176bb72790b91348e236258ad5c9d747cf19
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-04-22 18:45:42 +0200

    alle features anzeigen

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -32,7 +32,7 @@ let awsUrl = "https://aws.openweb.cc/stations";
 let aws = L.geoJson.ajax(awsUrl, {
     filter: function (feature) {
         console.log("Feature in filter: ", feature);
-        return feature.geometry.coordinates[2] > 3000;
+        // return feature.geometry.coordinates[2] > 100;
     },
     pointToLayer: function (point, latlng) {
         // console.log("point: ", point);
