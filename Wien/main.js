7be55dfc56cad4ad30ac32a5d334b710abd50d3b
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-04-22 11:19:50 +0200

    beautify

--- Wien/main.js
+++ Wien/main.js
@@ -46,8 +46,7 @@ let sights = L.geoJson.ajax(sightUrl, { //Punkte als Marker setzen
         });
         // console.log("Point", point);
         marker.bindPopup(`<h3>${point.properties.NAME}</h3>  
-        <p><a target="links" href="${point.properties.WEITERE_INF}">weiterführende Informationen</a></p>
-        `);
+        <p><a target="links" href="${point.properties.WEITERE_INF}">weiterführende Informationen</a></p>`);
         return marker;
     }
 });
