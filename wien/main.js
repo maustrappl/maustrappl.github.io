69b37ee46181f6eeccd813a8be67f05ea16ebbf2
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-04-23 15:53:33 +0200

    aws on data loaded

--- wien/main.js
+++ wien/main.js
@@ -1,8 +1,8 @@
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 
 let map = L.map("map", {
-    center: [48.208333, 16.373056],
-    zoom: 12,
+    // center: [48.208333, 16.373056],
+    // zoom: 12,
     layers: [
         startLayer
     ]
