134ab0be60ca7a422bf86136141068de335bcab6
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-04-26 17:56:17 +0200

    rainwievereingefügt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -154,3 +154,14 @@ aws.on("data:loaded", function () {
 
     //console.log(COLORS);
 });
+
+L.control.rainviwer({ 
+    position: 'bottomleft',
+    nextButtonText: '>',
+    playStopButtonText: 'Play/Stop',
+    prevButtonText: '<',
+    positionSliderLabelText: "Hour:",
+    opacitySliderLabelText: "Opacity:",
+    animationInterval: 500,
+    opacity: 0.5
+}).addTo(map);
