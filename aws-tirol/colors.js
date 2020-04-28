8eb90e005603f42091941433c2b7801ff5e5bfb5
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-04-28 13:47:37 +0200

    colors anpassen

--- aws-tirol/colors.js
+++ aws-tirol/colors.js
@@ -32,57 +32,57 @@ const COLORS = {
         ]
         };
 
-        //     snowhight: [ <
-        //         rect class = "legend-rect"
-        //         width = "41"
-        //         height = "18"
-        //         x = "0"
-        //         y = "0"
-        //         fill = "rgb(204,12,232)" > < /rect><rect class="legend-rect" width="41" height="18" x="0" y="18" fill="rgb(120,75,255)"></rect > < rect class = "legend-rect"
-        //         width = "41"
-        //         height = "18"
-        //         x = "0"
-        //         y = "36"
-        //         fill = "rgb(3,91,190)" > < /rect><rect class="legend-rect" width="41" height="18" x="0" y="54" fill="rgb(4,129,255)"></rect > < rect class = "legend-rect"
-        //         width = "41"
-        //         height = "18"
-        //         x = "0"
-        //         y = "72"
-        //         fill = "rgb(3,205,255)" > < /rect><rect class="legend-rect" width="41" height="18" x="0" y="90" fill="rgb(140,255,255)"></rect > < rect class = "legend-rect"
-        //         width = "41"
-        //         height = "18"
-        //         x = "0"
-        //         y = "108"
-        //         fill = "rgb(176,255,188)" > < /rect><rect class="legend-rect" width="41" height="18" x="0" y="126" fill="rgb(255,255,179)"></rect > < rect class = "legend-rect"
-        //         width = "41"
-        //         height = "18"
-        //         x = "0"
-        //         y = "144"
-        //         fill = "rgb(255,255,254)" > < /rect><line class="legend-border" x1="0" x2="41" y1="0" y2="0"></line > < line class = "legend-border"
-        //         x1 = "0"
-        //         x2 = "41"
-        //         y1 = "162"
-        //         y2 = "162" > < /line><text class="legend-text" x="20.5" y="18" dy="0.35em" font-size="12" text-anchor="middle">400</text > < text class = "legend-text"
-        //         x = "20.5"
-        //         y = "36"
-        //         dy = "0.35em"
-        //         font - size = "12"
-        //         text - anchor = "middle" > 300 < /text><text class="legend-text" x="20.5" y="54" dy="0.35em" font-size="12" text-anchor="middle">200</text > < text class = "legend-text"
-        //         x = "20.5"
-        //         y = "72"
-        //         dy = "0.35em"
-        //         font - size = "12"
-        //         text - anchor = "middle" > 100 < /text><text class="legend-text" x="20.5" y="90" dy="0.35em" font-size="12" text-anchor="middle">50</text > < text class = "legend-text"
-        //         x = "20.5"
-        //         y = "108"
-        //         dy = "0.35em"
-        //         font - size = "12"
-        //         text - anchor = "middle" > 25 < /text><text class="legend-text" x="20.5" y="126" dy="0.35em" font-size="12" text-anchor="middle">10</text > < text class = "legend-text"
-        //         x = "20.5"
-        //         y = "144"
-        //         dy = "0.35em"
-        //         font - size = "12"
-        //         text - anchor = "middle" > 1 < /text>]
+            snowhight: [ <
+                rect class = "legend-rect"
+                  
+                  
+                x = "0"
+                y = "0"
+                fill = "rgb(204,12,232)" > < /rect><rect class="legend-rect" width="41" height="18" x="0" y="18" fill="rgb(120,75,255)"></rect > < rect class = "legend-rect"
+                  
+                  
+                x = "0"
+                y = "36"
+                fill = "rgb(3,91,190)" > < /rect><rect class="legend-rect" width="41" height="18" x="0" y="54" fill="rgb(4,129,255)"></rect > < rect class = "legend-rect"
+                  
+                  
+                x = "0"
+                y = "72"
+                fill = "rgb(3,205,255)" > < /rect><rect class="legend-rect" width="41" height="18" x="0" y="90" fill="rgb(140,255,255)"></rect > < rect class = "legend-rect"
+                  
+                  
+                x = "0"
+                y = "108"
+                fill = "rgb(176,255,188)" > < /rect><rect class="legend-rect" width="41" height="18" x="0" y="126" fill="rgb(255,255,179)"></rect > < rect class = "legend-rect"
+                  
+                  
+                x = "0"
+                y = "144"
+                fill = "rgb(255,255,254)" > < /rect><line class="legend-border" x1="0" x2="41" y1="0" y2="0"></line > < line class = "legend-border"
+                x1 = "0"
+                x2 = "41"
+                y1 = "162"
+                y2 = "162" > < /line><text class="legend-text" x="20.5" y="18" dy="0.35em" font-size="12" text-anchor="middle">400</text > < text class = "legend-text"
+                x = "20.5"
+                y = "36"
+                dy = "0.35em"
+                font - size = "12"
+                   300 < /text><text class="legend-text" x="20.5" y="54" dy="0.35em" font-size="12" text-anchor="middle">200</text > < text class = "legend-text"
+                x = "20.5"
+                y = "72"
+                dy = "0.35em"
+                font - size = "12"
+                   100 < /text><text class="legend-text" x="20.5" y="90" dy="0.35em" font-size="12" text-anchor="middle">50</text > < text class = "legend-text"
+                x = "20.5"
+                y = "108"
+                dy = "0.35em"
+                font - size = "12"
+                   25 < /text><text class="legend-text" x="20.5" y="126" dy="0.35em" font-size="12" text-anchor="middle">10</text > < text class = "legend-text"
+                x = "20.5"
+                y = "144"
+                dy = "0.35em"
+                font - size = "12"
+                   1 < /text>]
 
-        //     ]
-        // };
+            ]
+        };
