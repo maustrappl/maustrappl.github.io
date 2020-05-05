41f99a2b2034d4abadd8ad96aa65d135cd8a702a
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-05-05 15:52:23 +0200

    hütten eingetragen

--- adlerweg/einkehr.js
+++ adlerweg/einkehr.js
@@ -1,5 +1,40 @@
 const EINKEHR = [
-    [ Etappennummer (9), Einkehrmöglichkeit, Lat, Lng ],
-    [ Etappennummer (1-33), Einkehrmöglichkeit, Lat, Lng ],
+    [ Etappennummer (9), Lamsenjochhütte, 47.38005, 11.60356],
+    [ Etappennummer (9), Binsalm, 47.5005, 11.60356],
+    [ Etappennummer (9), Engalm, 47.36005, 11.60356],
+    [ Etappennummer (9), Falkenhütte, 47.36005, 11.60356],
+
+   
+
+
+    [ Etappennummer (10),Falkenhütte, Lat, Lng ],
+    [ Etappennummer (10), Karwendelhaus, Lat, Lng ],
+    [ Etappennummer (11), Karwendelhaus, Lat, Lng ],
+    [ Etappennummer (11), Kastenalm, Lat, Lng ],
+    [ Etappennummer (11), Hallerangerhaus, Lat, Lng ],
+    [ Etappennummer (11), Hallerangeralm, Lat, Lng ],
     
+    [ Etappennummer (12), Hallerangerhaus, Lat, Lng ],
+    [ Etappennummer (12), Hallerangeralm, Lat, Lng ],
+    [ Etappennummer (12), Pfeishütte, Lat, Lng ],
+    [ Etappennummer (12), Bergstation Hafelekar, Lat, Lng ],
+  
+  
+  
+    [ Etappennummer (13),Patscherkofelhaus, Lat, Lng ],
+    [ Etappennummer (13), Boschebenalm, Lat, Lng ],
+    [ Etappennummer (13), Tulfeinalm, Lat, Lng ],
+   
+    [ Etappennummer (14), Solsteinhaus, Lat, Lng ],
+
+
+
+
+
+
+
+
+
+
+
 ];
