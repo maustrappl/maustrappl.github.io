e5774afa1f87fa8fd2a3a929493bd051018858a2
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-05-22 11:15:52 +0200

    lat lng angepasst

--- Tauernhoch/main.js
+++ Tauernhoch/main.js
@@ -1,7 +1,7 @@
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 
 let map = L.map("map", {
-    center: [48.208333, 16.373056],
+    center: [47.17971944, 12.181495],
     zoom: 12,
     layers: [
         startLayer
