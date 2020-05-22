6a416ee099e15a22db3176fe8f6e2cfdbef314f7
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-05-22 12:29:48 +0200

    ebike tracks als json eingefügt

--- Tauernhoch/main.js
+++ Tauernhoch/main.js
@@ -2,20 +2,17 @@ let startLayer = L.tileLayer.provider("BasemapAT.grau");
 
 let map = L.map("map", {
     center: [47.17971944, 12.181495],
-    zoom: 10,
+    zoom: 11,
     layers: [
         startLayer
     ]
 });
 
 L.control.layers({
-    "BasemapAT.grau": startLayer,
-    "BasemapAT": L.tileLayer.provider("BasemapAT"),
+    "BasemapAT": startLayer,
     "BasemapAT.highdpi": L.tileLayer.provider("BasemapAT.highdpi"),
     "BasemapAT.terrain": L.tileLayer.provider("BasemapAT.terrain"),
-    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
     "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
-    "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
     "BasemapAT.orthofoto+overlay": L.layerGroup([
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
