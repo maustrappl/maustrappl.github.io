90fbf811474b2311a2921c029ecf5836f074efed
Jakob Fink <jakob.fink@student.uibk.ac.at>
2020-05-22 10:03:12 +0200

    basemap vorlage eingefügt

---
+++ Tauernhoch/main.js
@@ -0,0 +1,14 @@
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+L.control.layers({
+    "BasemapAT.grau": startLayer,
+    "BasemapAT": L.tileLayer.provider("BasemapAT"),
+    "BasemapAT.highdpi": L.tileLayer.provider("BasemapAT.highdpi"),
+    "BasemapAT.terrain": L.tileLayer.provider("BasemapAT.terrain"),
+    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
+    "BasemapAT.orthofoto+overlay": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ])
+}.addTo(map);
